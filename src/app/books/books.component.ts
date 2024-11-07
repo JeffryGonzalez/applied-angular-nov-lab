77745cf42f49cdc2b0f2c0e9961c858bba05f86d
Jeffry Gonzalez <jeff@hypertheory.com>
2024-11-07 13:37:23 -0500

    books to table

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,15 +1,50 @@
 import { JsonPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  inject,
+  Signal,
+} from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
 
+type Book = {
+  id: string;
+  title: string;
+  author: string;
+  year: number;
+};
 @Component({
   selector: 'app-books',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [JsonPipe],
-  template: ` <pre>{{ books() | json }}</pre> `,
+  template: `<div class="overflow-x-auto">
+    <table class="table table-zebra">
+      <!-- head -->
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Year</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- row 1 -->
+        @for (book of books(); track book.id) {
+          <tr>
+            <th>{{ book.id }}</th>
+            <td>{{ book.author }}</td>
+            <td>{{ book.title }}</td>
+            <td>{{ book.year }}</td>
+          </tr>
+        }
+        <!-- row 2 -->
+      </tbody>
+    </table>
+  </div> `,
   styles: ``,
 })
 export class BooksComponent {
@@ -18,7 +53,7 @@ export class BooksComponent {
   books = toSignal(
     this.#http
       .get<{
-        data: { id: string; title: string; author: string };
+        data: Book[];
       }>('/api/books')
       .pipe(map((m) => m.data)),
   );
