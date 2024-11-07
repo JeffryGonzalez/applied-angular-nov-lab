ccb7af6566a3757469764edabf6f50edcd51833e
kevin <kevin_luo@progressive.com>
2024-11-07 16:13:43 -0500

    with century you can tell i care less with complete disregard for type safety and wished everything was type any and could just 'trust me bro the types are correct' it

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,9 +1,14 @@
 import { JsonPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  inject,
+  computed,
+} from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { map } from 'rxjs';
+import { first, map } from 'rxjs';
 
 @Component({
   selector: 'app-books',
@@ -12,30 +17,45 @@ import { map } from 'rxjs';
   imports: [RouterOutlet, JsonPipe],
   template: `
     <div class="overflow-x-auto">
-  <table class="table">
-    <!-- head -->
-    <thead>
-      <tr>
-        <th></th>
-        <th>id</th>
-        <th>title</th>
-        <th>author</th>
-        <th>year</th>
-      </tr>
-    </thead>
-    <tbody>
-      @for(book of books(); track book.id){
-        <tr>
-        <td>{{book.id}}</td>
-        <td>{{book.title}}</td>
-        <td>{{book.author}}</td>
-        <td>{{book.year}}</td>
-      </tr>
-      
-      }
-    </tbody>
-  </table>
-</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Century</th>
+            <th>Number of Books</th>
+          </tr>
+        </thead>
+        <tbody>
+          @for (bookBucket of bookBuckets(); track bookBucket.century) {
+            <tr>
+              <th>{{bookBucket.century}}</th>
+              <th>{{bookBucket.count}}</th>
+            </tr>
+          }
+        </tbody>
+        <table class="table">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th></th>
+              <th>id</th>
+              <th>title</th>
+              <th>author</th>
+              <th>year</th>
+            </tr>
+          </thead>
+          <tbody>
+            @for (book of books(); track book.id) {
+              <tr>
+                <td>{{ book.id }}</td>
+                <td>{{ book.title }}</td>
+                <td>{{ book.author }}</td>
+                <td>{{ book.year }}</td>
+              </tr>
+            }
+          </tbody>
+        </table>
+      </table>
+    </div>
   `,
   styles: ``,
 })
@@ -48,4 +68,30 @@ export class BooksComponent {
       }>('/api/books')
       .pipe(map((res) => res.data)),
   );
+
+  bookBuckets = computed(() => {
+    // lazy typescript type coalescing
+
+    const earliest = this.books()?.flatMap((b) => b.year).sort()[0] ?? 0;
+    const latestYear = this.books()?.flatMap((b) => b.year).sort().reverse()[0] ?? 0;
+
+    const result: BookBucketItem[] = [];
+    for (let i = +earliest/100; i <= latestYear/100; i++) {
+      result.push({ century: ~~i + '00', count: 0 });
+    }
+    console.log(result);
+
+    this.books()?.forEach((book) => {
+      // lazy typescript type coalescing
+      const bucket = ~~(book.year/100) + '00';
+      console.log(bucket);
+      result.find(bookBucket => bookBucket.century == bucket)!.count++;
+    });
+    return result;
+  });
 }
+
+type BookBucketItem = {
+  century: string;
+  count: number;
+};
